d1c737e14ea18e7f41f5e32192648175b4b66020
Nirmal Kumar Ahirwar <53752039+NirmalKAhirwar@users.noreply.github.com>
2023-05-15 21:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (75.62%) - LeetSync

---
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int start=0;
+        int end=m*n-1;
+        while(start<=end){
+            int mid=(start+end)>>1;
+            int row = mid / n;
+            int col = mid % n;
+            if(matrix[row][col]==target){
+                return true;
+            }else if(matrix[row][col]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return false;
+    }
+};
