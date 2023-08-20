47f194b931c999fbfbf260bf6afe4e1d94f6d8c3
Nirmal Kumar Ahirwar <53752039+NirmalKAhirwar@users.noreply.github.com>
2023-08-20 19:24:23 +0530

    Time: 27 ms (32.87%) | Memory: 20.2 MB (16.31%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,25 +1,54 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &mem, string s, int i, int j)
-    {
-        if(i >= j) return 1;
+    // int solve(vector<vector<int>> &mem, string s, int i, int j)
+    // {
+    //     if(i >= j) return 1;
 
-        if(mem[i][j] >= 0) return mem[i][j];
+    //     if(mem[i][j] >= 0) return mem[i][j];
 
-        return mem[i][j] = (s[i] == s[j])? solve(mem, s, i+1, j-1) : 0;
-    }
+    //     return mem[i][j] = (s[i] == s[j])? solve(mem, s, i+1, j-1) : 0;
+    // }
+    // int countSubstrings(string s) {
+    //     vector<vector<int>> mem(s.size(), vector<int>(s.size(), -1));
+    //     int c = 0;
+    //     int n = s.length();
+    //     for(int i=0; i < n; i++ )
+    //     {
+    //         for(int j=i; j < n; j++)
+    //         {
+    //             c += solve(mem, s, i, j);
+    //         }
+    //     }
+
+    //     return c;
+    // }
     int countSubstrings(string s) {
-        vector<vector<int>> mem(s.size(), vector<int>(s.size(), -1));
-        int c = 0;
-        int n = s.length();
-        for(int i=0; i < n; i++ )
+        vector<vector<int>> tab(s.size()+1, vector<int>(s.size()+1));
+        int count = 0;
+        for (int i = s.size() - 1; i >= 0; --i)
         {
-            for(int j=i; j < n; j++)
+            for (int j = i; j < s.size(); ++j)
             {
-                c += solve(mem, s, i, j);
+                if (i == j)
+                {
+                    tab[i][j] = 1;
+                    // cout << s.substr(i, j) << endl;
+                }
+                else if (i + 1 == j)
+                {
+                    tab[i][j] = s[i] == s[j] ? 1 : 0;
+                    // cout << s.substr(i, j) << endl;
+                }
+                else
+                {
+                    tab[i][j] = s[i] == s[j] ? tab[i + 1][j - 1] : 0;
+                    // cout << s.substr(i, j) << endl;
+                }
+                count += tab[i][j];
             }
         }
 
-        return c;
+        return count;
     }
+    
 };
